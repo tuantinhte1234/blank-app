38961415982b4c1ec08a27a43afc790117b8727c
David-FPI <156213645+David-FPI@users.noreply.github.com>
2024-09-16 00:27:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,6 @@
 
 ## About Me
 
-👋 Hi there! I'm **Sushovan Saha**, a **Machine Learning (ML)** enthusiast specializing in **Natural Language Processing (NLP)** and Computer Vision (CV). I did my M.Tech in **Data Science** from **IIT Guwahati**. I am also a **Kaggle Notebook Expert**.
-
-🌟 I'm passionate about exploring the possibilities of ML to solve real-world problems and improve people's lives. I love working on challenging projects that require me to stretch my abilities and learn new things.
-
-📚 In my free time, I like to contribute in [Kaggle](https://www.kaggle.com/sushovansaha9), Write ML blogs in [Medium](https://medium.com/@sushovansaha95), read ML related blogs and updates. I'm always looking for ways to stay up-to-date with the latest developments in the field.
 
 
 
