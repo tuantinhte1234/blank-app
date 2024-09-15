1b687e22ba1eb06ad066d0daf20cfcea196cf6aa
David-FPI <156213645+David-FPI@users.noreply.github.com>
2024-09-16 00:24:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,40 +14,7 @@ import streamlit as st
 📚 In my free time, I like to contribute in [Kaggle](https://www.kaggle.com/sushovansaha9), Write ML blogs in [Medium](https://medium.com/@sushovansaha95), read ML related blogs and updates. I'm always looking for ways to stay up-to-date with the latest developments in the field.
 
 
-### My Skills : 
-[![My Skills](https://skillicons.dev/icons?i=cpp,python,pytorch,vscode,git,github,docker&perline=8)](https://skillicons.dev)
 
-
-<img height=50 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/anaconda/anaconda-original.svg"/><img height=50 src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Google_Colaboratory_SVG_Logo.svg/1280px-Google_Colaboratory_SVG_Logo.svg.png"/><img height=50 src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1200px-Jupyter_logo.svg.png"/><img height=50 src="https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/189_Kaggle-512.png"/><img height=50 src="https://github.com/ambideXtrous9/ambideXtrous9/blob/main/icons/huggingface.png?raw=true"/><img height=50 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Keras_logo.svg/1200px-Keras_logo.svg.png"/><img height=50 src="https://github.com/ambideXtrous9/ambideXtrous9/blob/main/icons/lightning.png?raw=true"/><img height=50 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg"/>
-
-
-
-Thank you for visiting my GitHub profile!
-
-![Sushovan's GitHub stats](https://github-readme-stats.vercel.app/api?username=ambideXtrous9&show_icons=true&theme=dracula)
-
-
-
-<div id="badges">
-  <a href="https://in.linkedin.com/in/sushovan-saha-29a00a113">
-    <img src="https://img.shields.io/badge/LinkedIn-0A66C2.svg?style=for-the-badge&logo=LinkedIn&logoColor=white"/>
-  </a>
-  <a href="https://www.kaggle.com/sushovansaha9">
-    <img src="https://img.shields.io/badge/Kaggle-20BEFF.svg?style=for-the-badge&logo=Kaggle&logoColor=white"/>
-  </a>
-  <a href="https://leetcode.com/ambideXtrous9/">
-    <img src="https://img.shields.io/badge/LeetCode-FFA116.svg?style=for-the-badge&logo=LeetCode&logoColor=white"/>
-  </a>
-  <a href="https://www.instagram.com/ambidextrous_9/">
-    <img src="https://img.shields.io/badge/Instagram-E4405F.svg?style=for-the-badge&logo=Instagram&logoColor=white"/>
-  </a>
-  <a href="https://www.facebook.com/sushovan.saha.16/">
-    <img src="https://img.shields.io/badge/Facebook-1877F2.svg?style=for-the-badge&logo=Facebook&logoColor=white"/>
-  </a>
-</div>
-
-
-![](https://komarev.com/ghpvc/?username=ambideXtrous9)
 
 
 ### My Medium Blogs
